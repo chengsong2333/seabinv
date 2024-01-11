4a8da6e69c2489e4a291cacb2f1d0e0e6c22f68f
Cheng Song <sc951212@gmail.com>
2024-01-12 08:02:30 +0900

    PT

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,3 @@ from seabinv.Models import Model, ModelMatrix
 from seabinv.SingleChain import SingleChain
 from seabinv.mcmcOptimizer import MCMC_Optimizer
 from seabinv.Plotting import PlotFromStorage
-from seabinv.SynthObs import SynthObs
